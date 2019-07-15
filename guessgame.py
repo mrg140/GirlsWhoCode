822d114eb93601abed201ff87ae5c07144e9ec5d
mrg140 <mariareyesg140@gmail.com>
2019-07-15 14:55:00 -0400

    New

--- guessgame.py
+++ guessgame.py
@@ -5,19 +5,21 @@ from random import *
 aRandomNumber = randint(1, 20)
 tries = 0
 # For Testing: print(aRandomNumber)
-while tries < 3:
+#while tries < 3:
+for tries in range(3):
 	guess = input("Guess a number between 1 and 20 (inclusive): ")
 	if not guess.isnumeric(): # checks if a string is only digits 0 to 9
 		print("That's not a positive whole number, try again!")
 	else:
 		guess = int(guess)
-		tries += 1
-			print ("Try Again")
 # converts a string to an integer
 	if guess == aRandomNumber:
 		print("Correct!")
 		break
 	elif guess < aRandomNumber:
 		print ("Too low!")
+		tries +=1
 	else:
 		print ("Too high!")
+if tries == 3:
+	print ("Try Again!")
